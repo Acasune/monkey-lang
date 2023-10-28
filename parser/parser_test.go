df2eac3158beda6d44902ba45937ac5aa130b172
acasune <tttt4aaaa@gmail.com>
2023-10-28 11:45:46 +0900

    add infix precedence test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"monkey-lang/ast"
 	"monkey-lang/lexer"
+	"strings"
 	"testing"
 )
 
@@ -230,6 +231,75 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 		if !testIntegerLiteral(t, exp.Right, tt.rightValue) {
 			return
+		}
+	}
+}
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input string
+		expected string
+	} {
+		{
+			"-a * b",
+			"((-a) * b)",
+		},
+		{
+			"!-a",
+			"(!(-a))",
+		},
+		{
+			"a + b + c",
+			"((a + b) + c)",
+		},
+		{
+			"a + b - c",
+			"((a + b) - c)",
+		},
+		{
+			"a * b * c",
+			"((a * b) * c)",
+		},
+		{
+			"a * b / c",
+			"((a * b) / c)",
+		},
+		{
+			"a + b / c",
+			"(a + ( b / c))",
+		},
+		{
+			"a + b * c + d / e - f",
+			"(((a + ( b * c))  + (d / e)) - f)",
+		},
+		{
+			"3 + 4; -5 * 5",
+			"(3 + 4)((-5) * 5)",
+		},
+		{
+			"5 > 4 == 3 < 4",
+			"((5 > 4) == (3 < 4))",
+		},
+		{
+			"5 < 4 != 3 > 4",
+			"((5 < 4) != (3 > 4))",
+		},
+		{
+			"3 + 4 * 5 == 3 * 1 + 4 * 5",
+			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+		
+		actual := program.String()
+		expected := strings.ReplaceAll(tt.expected, " ", "")
+		if actual != expected {
+			t.Errorf("expected=%q, got=%q",expected, actual)
 		}
 	}
 }
