1e3dd4ade0709d0bbde86a71db40575c310a1b6b
acasune <tttt4aaaa@gmail.com>
2023-11-03 09:15:06 +0900

    add evaluator for repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey-lang/evaluator"
 	"monkey-lang/lexer"
 	"monkey-lang/parser"
 )
@@ -41,8 +42,12 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
