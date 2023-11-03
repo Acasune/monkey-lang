715e82cb09065698ebeaf3aa596cde56361bbfa4
acasune <tttt4aaaa@gmail.com>
2023-11-03 10:19:38 +0900

    add infix evaluator for comp

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -66,6 +66,10 @@ func evalInfixExpression(
 		switch {
 		case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 			return evalIntegerInfixExpression(operator, left, right)
+		case operator == "==":
+			return nativeBooleanObject(left == right)
+		case operator == "!=":
+			return nativeBooleanObject(left != right)
 		default:
 			return NULL
 		}
@@ -87,6 +91,14 @@ func evalIntegerInfixExpression(
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
 	}
