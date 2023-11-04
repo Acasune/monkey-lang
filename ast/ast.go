4e161dd6cbb5e53ba6fe610d7ba0fe7e0eec16d0
acasune <tttt4aaaa@gmail.com>
2023-11-04 09:33:22 +0900

    fix parser for array literal and introducing evaluator for array

--- ast/ast.go
+++ ast/ast.go
@@ -275,12 +275,12 @@ func (ce *CallExpression) String() string {
 }
 
 type ArrayLiteral struct {
-	Token token.Token
+	Token    token.Token
 	Elements []Expression
 }
 
-func (al *ArrayLiteral) expressionNode() {}
-func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal}
+func (al *ArrayLiteral) expressionNode()      {}
+func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
 func (al *ArrayLiteral) String() string {
 	var out bytes.Buffer
 
@@ -294,4 +294,25 @@ func (al *ArrayLiteral) String() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type IndexExpression struct {
+	Token token.Token
+	Left  Expression
+	Index Expression
+}
+
+func (ie *IndexExpression) expressionNode()      {}
+func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString("[")
+	out.WriteString(ie.Index.String())
+	out.WriteString("]")
+
+	return out.String()
+
 }
